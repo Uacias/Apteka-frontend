fbc9396e9208a573b220d878d2144dbfcdcb6079
Filip.Gumula <filgum0326@gmail.com>
2023-05-26 12:45:12 +0200

    Navigation rebuilding - fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,44 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { ContactComponent } from './pages/about/subpages/contact/contact.component';
+import { LocationComponent } from './pages/about/subpages/location/location.component';
+import { TermsAndConditionsComponent } from './pages/about/subpages/terms-and-conditions/terms-and-conditions.component';
+import { OurStoryComponent } from './pages/about/subpages/our-story/our-story.component';
+import { PrivacyPolicyComponent } from './pages/about/subpages/privacy-policy/privacy-policy.component';
+
+import { TeaAndCoffeeComponent } from './pages/store/subpages/tea-and-coffee/tea-and-coffee.component';
+import { VitaminsComponent } from './pages/store/subpages/vitamins/vitamins.component';
+import { PharmacyComponent } from './pages/store/subpages/pharmacy/pharmacy.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
+
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: [
+      { path: 'location', component: LocationComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'our-story', component: OurStoryComponent },
+      { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
+      { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    ],
+  },
+  {
+    path: 'store',
+    component: AboutComponent,
+    children: [
+      { path: 'pharmacy', component: PharmacyComponent },
+      { path: 'health-and-wellness', component: HealthAndWellnessComponent },
+      { path: 'beauty', component: BeautyComponent },
+      { path: 'supplements', component: SupplementsComponent },
+      { path: 'vitamins', component: VitaminsComponent },
+      { path: 'diet-and-nutrition', component: DietAndNutritionComponent },
+      { path: 'tea-and-coffee', component: TeaAndCoffeeComponent },
+    ],
+  },
+
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];

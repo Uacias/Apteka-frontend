d4e847f533e89b7dedcdd7041aea0be8d728e11d
Filip.Gumula <filgum0326@gmail.com>
2023-05-27 15:35:01 +0200

    guard for paths: /about, /store

---
+++ src/app/shared/guards/redirected-pages.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RedirectedPagesGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (state.url === '/about' || state.url === '/store') {
+      return this.router.parseUrl('/home');
+    }
+    return true;
+  }
+}

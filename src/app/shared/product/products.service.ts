54182a0a8dcb6233f58580a1d15bd8c6c52aed85
Filip.Gumula <filgum0326@gmail.com>
2023-05-28 23:22:01 +0200

    url fix

--- src/app/shared/product/products.service.ts
+++ src/app/shared/product/products.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
 import { Observable } from 'rxjs';
@@ -7,26 +7,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ProductsService {
-  url: string = 'localhost:8080/produkty/';
+  url: string = 'http://localhost:8080/produkty/';
+
   constructor(private readonly http: HttpClient) {}
 
   getAllProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(`${this.url}getAll`);
+    return this.http.get<IProduct[]>(`${this.url}getAll`, {});
   }
 
   getProduct(id: number): Observable<IProduct> {
-    return this.http.get<IProduct>(`${this.url}${id}`);
+    return this.http.get<IProduct>(`${this.url}${id}`, {});
   }
 
   addProduct(id: number, product: IProduct): Observable<IProduct> {
-    return this.http.post<IProduct>(`${this.url}add/${id}`, product);
+    return this.http.post<IProduct>(`${this.url}add/${id}`, product, {});
   }
 
   editProduct(id: number, product: IProduct): Observable<IProduct> {
-    return this.http.put<IProduct>(`${this.url}${id}`, product);
+    return this.http.put<IProduct>(`${this.url}${id}`, product, {});
   }
 
   deleteProduct(id: number): Observable<IProduct> {
-    return this.http.delete<IProduct>(`${this.url}${id}`);
+    return this.http.delete<IProduct>(`${this.url}${id}`, {});
   }
 }

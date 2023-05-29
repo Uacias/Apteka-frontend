6b2ff5389c9b79dfd6d63de043b88cff8c18aba4
Filip.Gumula <filgum0326@gmail.com>
2023-05-29 14:27:33 +0200

    customers opinions section to homepage, modified navbar, added responsive navbar and customers section

---
+++ src/app/pages/home/user-opinions/user-opinions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-opinions',
+  templateUrl: './user-opinions.component.html',
+  styleUrls: ['./user-opinions.component.scss']
+})
+export class UserOpinionsComponent {
+
+}

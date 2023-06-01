0f3bc9c4351e260c122b5d47349459153914db8d
Filip.Gumula <filgum0326@gmail.com>
2023-06-01 14:13:06 +0200

    added quantity, totalprice for product, modified fetching by type from backend in product.service

--- src/app/shared/product/product.component.ts
+++ src/app/shared/product/product.component.ts
@@ -4,8 +4,26 @@ import { IProduct } from './product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
   @Input() product!: IProduct;
+  quantity: number = 1;
+  totalPrice: number = this.product?.price ?? 0;
+
+  incrementQuantity(): void {
+    this.quantity++;
+    this.calculateTotalPrice();
+  }
+
+  decrementQuantity(): void {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.calculateTotalPrice();
+    }
+  }
+
+  private calculateTotalPrice(): void {
+    this.totalPrice = this.product.price * this.quantity;
+  }
 }
